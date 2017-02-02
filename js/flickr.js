bac547be837cea140190febfced14495878cb14d
Seif <seif.dorani@gmail.com>
2017-02-02 12:21:30 -0500

    creating event handler with jquery, while using click method exec an annom func

---
+++ js/flickr.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){   //to make sure js code doesn't run before html is fully loaded
+  $('button').click();          //event handler, when button is clicked, program runs, 3 buttons, cat dog and moose
+                                //click method, another handler function, runs each time the buttons is clicked
+                                //callback function for click event
+})
