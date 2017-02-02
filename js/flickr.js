2f6e002adfba48ec793fbe2a69693b50c0fe80e2
Seif <seif.dorani@gmail.com>
2017-02-02 12:29:05 -0500

    using .getJSON method on the following arguments, next will declare them

--- js/flickr.js
+++ js/flickr.js
@@ -2,6 +2,7 @@ $(document).ready(function(){   //to make sure js code doesn't run before html i
   $('button').click(function(){
     $("button").removeClass("selected"); //remove highlight after click event
     $(this).addClass("selected");
+    $.getJSON(flickerAPI, flickOptions, displayPhotos);
   });
 })
 //event handler, when button is clicked, program runs, 3 buttons, cat dog and moose
@@ -10,3 +11,9 @@ $(document).ready(function(){   //to make sure js code doesn't run before html i
 //add a new class on clicked button
 //$this is a way to indicate the element that responds to the clicked event
 //$this refers to button
+
+
+//Ok, now we need to send an ajax request whenever a person clicks on a button:
+//so we will put the programing inside the click event
+//we will use .getJSON method that will take the following arguments
+//url to resource, the data we want to send along with the url, a callback function when flickr responds
