db303ecc12515e1cb0b71d70f82a91f74045cf56
Seif <seif.dorani@gmail.com>
2017-02-02 12:25:47 -0500

    each button highlight when clicked

--- js/flickr.js
+++ js/flickr.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){   //to make sure js code doesn't run before html is fully loaded
-  $('button').click();          //event handler, when button is clicked, program runs, 3 buttons, cat dog and moose
-                                //click method, another handler function, runs each time the buttons is clicked
-                                //callback function for click event
+  $('button').click(function(){
+    $("button").removeClass("selected"); //remove highlight after click event
+    $(this).addClass("selected");
+  });
 })
+//event handler, when button is clicked, program runs, 3 buttons, cat dog and moose
+//click method, another handler function, runs each time the buttons is clicked
+//callback function for click event
+//add a new class on clicked button
+//$this is a way to indicate the element that responds to the clicked event
+//$this refers to button
